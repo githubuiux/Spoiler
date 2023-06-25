6b5672a0152e75f2c0af6f7d8af5ca752d4fe393
githubuiux <uiuxista@gmail.com>
2023-06-25 16:23:24 +0300

    Initial Commit

---
+++ Spoiler/SpoilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoilerApp.swift
+//  Spoiler
+//
+//  Created by Levent on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpoilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
